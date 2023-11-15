c09734d81cf7e0af453a3d4aa3c29fa198d61e24
Vinicius de Moraes <vinicius.moraes@iscoolapp.com.br>
2023-11-15 17:56:16 -0300

    Foi corrigido alguns bug e agora o input de mensage está funcionando.

--- src/screens/Message.js
+++ src/screens/Message.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   View,
   Text,
@@ -14,10 +14,15 @@ import MessageUser from './MessageUser';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { theme } from '../core/theme';
 
+
+
 class Message extends Component {
 
   constructor(props) {
+
     super(props);
+
+
     this.state = {
       message: [
         {
@@ -40,20 +45,32 @@ class Message extends Component {
 
   render() {
     return (
-      <View style={styles.areaFeed}>
+      <View style={styles.container}>
+        <View style={styles.content}>
+        </View>
 
         <KeyboardAvoidingView
           style={styles.footer}
           behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
           keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}>
-          <View style={styles.textInput}>
-            <TextInput />
-          </View>
 
-          <Icon name="send-sharp"
-            size={26}
-            color={theme.colors.primary}
-            style={styles.fildMessage} />
+          <View style={styles.textInput}>
+            <TextInput
+              // style={{ height: inputHeight }}
+              value={msg}
+              multiline={true}
+
+            // onContentSizeChange={(e) => setInputHeight(e.nativeEvent.content.content.height + 12)}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={() => setMsg(msg)}
+          >
+            <Icon name="send-sharp"
+              size={26}
+              color={theme.colors.primary}
+              style={styles.fildMessage} />
+          </TouchableOpacity>
         </KeyboardAvoidingView>
 
       </View>
@@ -63,6 +80,8 @@ class Message extends Component {
 
 export default Message;
 
+//const [inputHeight, setInputHeight] = useState(0);
+const [msg, setMsg] = '';
 const styles = StyleSheet.create({
   currentUserMessage: {
     alignSelf: 'flex-end',
@@ -78,8 +97,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingHorizontal: 10,
   },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
   footer: {
-    padding: 20,
+    padding: 14,
     backgroundColor: theme.colors.textInput,
     borderTopColor: theme.colors.primary,
     borderTopWidth: 1,
@@ -88,15 +111,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   textInput: {
-    height: 35,
     borderRadius: 8,
     borderColor: theme.colors.primary,
     backgroundColor: theme.colors.textInput,
     flex: 1,
     borderWidth: 1,
     paddingHorizontal: 12,
-
-
+  },
+  fildMessage: {
+    marginLeft: 12
   }
 
 
